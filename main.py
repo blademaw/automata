f36ae788a36efa8bcbd11b1bf9bd996f689ce59b
blademaw <jackoli3@me.com>
2022-02-09 23:55:41 +1100

    Fix OOP, commenting

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Main PY file for [D|(ND)]FA Project
-# NOTE: Currently unrefined and an absolute WIP. Not functional atm.
+# NOTE: Currently unrefined and a WIP.
 # TODO: God, this is horrible as is; breaks major OOP practices — need to refactor:
     # Ensure FA theory is accurately reflected in objects
     # Add getters and setters for the love of god
-    # Fix typehinting.
+    # Migrate to numpy for arrays?
 import itertools, math
 
 class DFA():
@@ -14,7 +14,7 @@ class DFA():
         """
         Init function for DFA class.
 
-        :params: (un)sorted alphabet, number of transitions, transition function in matrix
+        :params: alphabet, number of transitions, transition function in matrix
         form [state][character], start state, all accept states.
         """
         self.alphabet = alphabet
@@ -24,9 +24,9 @@ class DFA():
         self.start = start_state
         self.accept = accept_states
 
-        # populate an array for state objects, with dictionary for faster access to alphabet indeces in trans_func
-        # transitions are thus accessed trans_func[state.name - 1][indeces[character]]
-            # states will be accessed states[state.name] (1-indexing)
+        # populate an array of state objects with dictionary for faster access to alphabet indices in trans_func
+        # transitions are thus accessed trans_func[state.name - 1][indices[character]]
+            # states will be accessed via states[state.name] (1-indexing)
         self.states = [None] * (self.num_states+1)
         
         # add start states, final states, intermediate states
@@ -36,15 +36,16 @@ class DFA():
             self.states[ac_state] = state(ac_state, self, 2)
         
         if self.start in self.accept:
-            self.states[self.start].state_type = 3 # TODO: Is this not redundant?
+            self.states[self.start].set_type(3)
         
         for i in range(1, self.num_states+1):
             if self.states[i] == None:
                 self.states[i] = state(i, self, 1)
 
-        self.state_indeces = {}
+        # record indices of labels
+        self.state_indices = {}
         for i in range(len(self.alphabet)):
-            self.state_indeces[self.alphabet[i]] = i
+            self.state_indices[self.alphabet[i]] = i
 
     
     def run_word(self, word:str) -> bool:
@@ -52,43 +53,25 @@ class DFA():
         Runs a word (checked to be a valid string over self.alphabet) through the DFA,
         returning True if accepted and False otherwise.
         """
-        # initial validation
+        # cut down words with nonexistant characters
         for char in word:
-            if char not in self.state_indeces:
+            if char not in self.state_indices:
                 return False
 
+        # migrate through DFA
         current = self.states[self.start]
         for char in word:
-            current = self.states[self.trans_func[current.name - 1][self.state_indeces[char]]]
+            current = self.states[self.trans_func[current.get_name() - 1][self.state_indices[char]]]
         
-        if current.state_type in (2, 3):
+        # accept if in accept-like state
+        if current.get_type() in (2, 3):
             return True
         return False
     
-    def print_contents(self):
-        return (self.alphabet, self.num_states, self.trans_func, self.start, self.accept)
-    
-    def strings(self, maximum:int) -> list:
-        """
-        Permutes and finds the first `maximum` strings over the specified alphabet described (accepted) by the FA
-        in lexicographic order (work in progress).
-        """
-        counter = 0
-        word_list = []
-        cur_repeat = 2
-        first = [""] + self.sorted_alph
-        iterable = first
-        while counter != maximum:
-            for word in iterable:
-                if self.run_word(word):
-                    word_list.append("".join(word))
-                    counter += 1
-                if counter >= maximum:
-                    break
-            iterable = itertools.product(first, repeat=cur_repeat)
-            cur_repeat += 1
 
-        return word_list
+    def print_contents(self):
+        """Prints contents of alphabet, number of states, transition matrix, start, and accept state(s)"""
+        return (self.alphabet, self.num_states, self.trans_func, self.start, self.accept)
 
     def strings_max(self, max_length:int, max_words:int=math.inf):
         """
@@ -97,29 +80,24 @@ class DFA():
         and maximum to append before halting. If max_words not given, will find all words of length
         <= <max_length> in language described by FA.
         """
-        lang = [""] + self.sorted_alph
+        # enumerate all possible strings in alphabet up to length
+        lang = [self.sorted_alph]
         for length in range(2, max_length+1):
             for word in itertools.product(self.sorted_alph, repeat=length):
                 lang.append(''.join(word))
-        
-        lang.sort()
-        print(lang)
 
+        # loop to find accepted words
         counter = 0
-        word_list = []
+        word_list = [] # accepted
         for word in lang:
             if self.run_word(word):
-                print(word)
-                # TODO: Fix this
                 if word not in word_list:
-                    word_list.append(word)
+                    word_list.append(word) # add if accepted
                     counter += 1
             if counter >= max_words:
                 break
 
         return sorted(word_list, key=len)
-    
-    #def transition_table(self):
         
 
 class state():
@@ -128,9 +106,32 @@ class state():
     2 = accept state, 3 = accept and start state.
     """
     def __init__(self, num, DFA, state_type):
+        """
+        Init function for state class.
+        
+        Inputs: number label, finite automata class, type of state
+        """
         self.name = num
         self.state_type = state_type
         self.state_DFA = DFA
+    
+    def set_type(self, state_type):
+        """
+        Function for setting/updating a state's type
+        """
+        self.state_type = state_type
+    
+    def get_type(self):
+        """
+        Function for getting a state's type
+        """
+        return self.state_type
+
+    def get_name(self):
+        """
+        Function for getting a state's label/name
+        """
+        return self.name
 
 
 
@@ -142,15 +143,19 @@ if __name__ == "__main__":
     start = int(input("Enter start state: "))
     final = [int(i) for i in (input("Enter accept states: ")).split(" ")]
     matr = [[None]*len(alph) for x in range(num)]
+
+    # create transition matrix
     print("Enter transition state(s) for the following states in the order of alphabet %s" % alph)
     for i in range(1, num+1):
         cur = input("%d: " % i).split()
         for s in range(len(alph)):
             matr[i-1][s] = int(cur[s])
-    print("Creating DFA.")
+    print("\n\nStrings:")
+    
     dfa_imp = DFA(alph, num, matr, start, final)
     print(dfa_imp.strings_max(6))
 
+    # TESTING
     # dfa_imp = DFA(['a', 'b'], 2, [[2, 1], [1, 2]], 1, [1])
     # print(dfa_imp.strings_max(4, 10))
     
